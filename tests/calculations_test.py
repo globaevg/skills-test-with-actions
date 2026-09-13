a02c6dbc8bcd2a991944fd34a2447ba2b4955e92
Yauhen <globaevg@gmail.com>
2026-09-13 15:53:20 +0200

    Updated test to correct value.

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -1,13 +1,13 @@
 # System Modules
-import sys
 import os
+import sys
 
 # Installed Modules
 import pytest
 
 # Project Modules
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
-from calculations import area_of_circle, get_nth_fibonacci   # noqa: E402
+from calculations import area_of_circle, get_nth_fibonacci  # noqa: E402
 
 
 def test_area_of_circle_positive_radius():
@@ -67,4 +67,4 @@ def test_get_nth_fibonacci_ten():
     result = get_nth_fibonacci(n)
 
     # Assert
-    assert result == 89
+    assert result == 55
